e4d70d762f2bd5799c4605f3975e6b1b3a9dfdc5
Ketan Gurav <54971822+itzketan@users.noreply.github.com>
2021-06-13 20:18:34 +0530

    Add files via upload

---
+++ 2nd day.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("30 days 30 hour challenge")
+print('30 days 30 hour challenge')
+
+
+# In[2]:
+
+
+hours = "two"
+hours
+
+
+# In[3]:
+
+
+hours ="two"
+print(hours)
+
+
+# In[4]:
+
+
+days = "two days"
+print(days[0])
+
+
+# In[5]:
+
+
+print(days[5])
+
+
+# In[6]:
+
+
+challenge = "I will win "
+print(challenge[7:10])
+
+
+# In[8]:
+
+
+print(challenge.lower())
+
+
+# In[10]:
+
+
+a = "30 days"
+b = "30 hours"
+c = a + b
+print(c)
+print(a+b)
+
+
+# In[13]:
+
+
+a = "30 days"
+b = "30 hours"
+c = a + "   " + b
+print(c)
+
+
+# In[28]:
+
+
+text = 'thirty days and thirty hours'
+x = text.casefold()
+print(x)
+
+
+# In[26]:
+
+
+text = " thirty days and thirty hours"
+x = text.upper()
+print(x)
+
+
+# In[29]:
+
+
+text = " thirty days and thirty hours"
+x = text.find('and')
+print(x)
+
+
+# In[30]:
+
+
+text = " thirty days and thirty hours"
+x = text.isalpha()
+print(x)
+
+
+# In[31]:
+
+
+text = " thirty days and thirty hours"
+x = text.isalnum()
+print(x)
+
+
+# In[ ]:
+
+
+
+
